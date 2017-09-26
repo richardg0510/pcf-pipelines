bde54503ad85625fab2b0ce30291678ee132e9e3
Greg Richardson <richardg0510@gmail.com>
2017-09-26 16:05:59 -0500

    Create task.sh

---
+++ upgrade-ops-manager/gcp/tasks/resize-opsman-disk/task.sh
@@ -0,0 +1,11 @@
+set -eu
+
+cat > cliaas-config/gcpcreds.json <<EOF
+${OPSMAN_GCP_CREDFILE_CONTENTS}
+EOF
+
+gcloud --project ${OPSMAN_PROJECT} auth activate-service-account --key-file cliaas-config/gcpcreds.json
+
+OPSMAN_EXTERNAL_IP=$(dig +short ${OPSMAN_DOMAIN_OR_IP_ADDRESS})
+OPSMAN_DISK_URI=$(gcloud --project ${OPSMAN_PROJECT} compute instances list --filter="networkInterfaces.accessConfigs.natIP=${OPSMAN_EXTERNAL_IP}" --format=json | jq '.[] .disks[] | select ( .boot == true ) .source')
+gcloud --project ${OPSMAN_PROJECT} compute disks resize ${OPSMAN_DISK_URI} --zone ${OPSMAN_ZONE1} --size=${OPSMAN_DISK_SIZE}
