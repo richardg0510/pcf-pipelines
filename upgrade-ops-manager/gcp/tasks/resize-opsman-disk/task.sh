fe4e6e0371c44cfdd3548b53e720e9d1a72fb382
Greg Richardson <greg.richardson@bsci.com>
2017-09-27 15:44:16 -0500

    Update task.sh

--- upgrade-ops-manager/gcp/tasks/resize-opsman-disk/task.sh
+++ upgrade-ops-manager/gcp/tasks/resize-opsman-disk/task.sh
@@ -10,4 +10,4 @@ gcloud --project ${OPSMAN_PROJECT} auth activate-service-account --key-file resi
 
 OPSMAN_EXTERNAL_IP=$(dig +short ${OPSMAN_DOMAIN_OR_IP_ADDRESS})
 OPSMAN_DISK_URI=$(gcloud --project ${OPSMAN_PROJECT} compute instances list --filter="networkInterfaces.accessConfigs.natIP=${OPSMAN_EXTERNAL_IP}" --format=json | jq '.[] .disks[] | select ( .boot == true ) .source')
-gcloud --project ${OPSMAN_PROJECT} compute disks resize ${OPSMAN_DISK_URI} --zone ${OPSMAN_ZONE1} --size=${OPSMAN_DISK_SIZE}
+gcloud --project ${OPSMAN_PROJECT} compute disks resize ${OPSMAN_DISK_URI} --zone ${OPSMAN_ZONE} --size=${OPSMAN_DISK_SIZE}
