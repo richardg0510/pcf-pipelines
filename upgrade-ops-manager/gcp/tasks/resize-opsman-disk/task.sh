c0969ab1bd4f0939d22cbb95d49b55cbfc184b8e
Greg Richardson <richardg0510@gmail.com>
2017-09-28 14:58:24 -0500

    Update task.sh

--- upgrade-ops-manager/gcp/tasks/resize-opsman-disk/task.sh
+++ upgrade-ops-manager/gcp/tasks/resize-opsman-disk/task.sh
@@ -2,14 +2,18 @@
 
 set -eu
 
-sleep 10
+OPSMAN_GCP_CREDFILE="./gcpcreds.json"
+OPSMAN_GCP_INSTANCE_INFO="./opsman.json"
 
-cat > gcpcreds.json <<EOF
+cat > ${OPSMAN_GCP_CREDFILE} <<EOF
 ${OPSMAN_GCP_CREDFILE_CONTENTS}
 EOF
 
-gcloud --project ${OPSMAN_PROJECT} auth activate-service-account --key-file gcpcreds.json
+gcloud --project ${OPSMAN_PROJECT} auth activate-service-account --key-file ${OPSMAN_GCP_CREDFILE}
 
 OPSMAN_EXTERNAL_IP=$(dig +short ${OPSMAN_DOMAIN_OR_IP_ADDRESS})
-OPSMAN_DISK_URI=$(gcloud --project ${OPSMAN_PROJECT} compute instances list --filter="networkInterfaces.accessConfigs.natIP=${OPSMAN_EXTERNAL_IP}" --format=json | jq '.[] .disks[] | select ( .boot == true ) .source')
-gcloud --project ${OPSMAN_PROJECT} compute disks resize ${OPSMAN_DISK_URI//\"} --zone ${OPSMAN_ZONE} --size=${OPSMAN_DISK_SIZE} --quiet
+gcloud --project ${OPSMAN_PROJECT} compute instances list --filter="networkInterfaces.accessConfigs.natIP=${OPSMAN_EXTERNAL_IP}" --format=json >> ${OPSMAN_GCP_INSTANCE_INFO}
+OPSMAN_NAME=$(jq --raw-output '.[] .name' ${OPSMAN_GCP_INSTANCE_INFO})
+OPSMAN_DISK_URI=$(jq --raw-output '.[] .disks[] | select ( .boot == true ) .source' ${OPSMAN_GCP_INSTANCE_INFO})
+gcloud --project ${OPSMAN_PROJECT} compute disks resize ${OPSMAN_DISK_URI} --zone ${OPSMAN_ZONE1} --size=${OPSMAN_DISK_SIZE} --quiet
+gcloud --project ${OPSMAN_PROJECT} compute instance reset ${OPSMAN_NAME} --zone ${OPSMAN_ZONE1} --quiet
